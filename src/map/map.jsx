d810a5ca681745179c5d91c71dc20aa14e79a009
Hyunkyu Yoon <hkyoon85@gmail.com>
2016-04-11 17:41:05 +0900

    Covert map.jsx to es6 syntax

--- src/map/map.jsx
+++ src/map/map.jsx
@@ -4,43 +4,32 @@ import Promise from 'q';
 import _ from 'lodash';
 
 import daumAPIWrapper from '../daum-api-wrapper';
+
 import {
-  getChildName,
   daumMapCoordsToArrayCoords,
   daumMapBoundsToMinMaxBounds,
 } from '../utils';
-import DAUM_BASE_MAP_TYPES, {
-  convertToDaumBaseMapType,
-} from './constants/base-map-types';
+
+import DAUM_BASE_MAP_TYPES, { convertToDaumBaseMapType } from './constants/base-map-types';
 import OverlayContainer from './overlay/container';
 import Bounds from '../datatypes/bounds';
 
-
-export default React.createClass({
-  displayName: 'ReactDaumMap::map',
-  propTypes: {
-    APIKey: React.PropTypes.string,
-    daumAPILoading: React.PropTypes.node,
-    daumAPILoadFailed: React.PropTypes.node,
-    position: React.PropTypes.array.isRequired,
-    onMove: React.PropTypes.func,
-    style: React.PropTypes.object,
-    baseMapType: React.PropTypes.oneOf([..._.keys(DAUM_BASE_MAP_TYPES), undefined]),
-    overlayMapTypes: React.PropTypes.array,
-    zoomLevel: React.PropTypes.number,
-  },
-  getInitialState() {
+export default class DaumMap extends React.Component {
+  constructor(props) {
+    super(props);
     if (this.props.APIKey) {
-      daumAPIWrapper.load(this.props.APIKey);
+      this.daumAPILoadPromise = daumAPIWrapper.load(this.props.APIKey);
     }
+
     const initDeferred = Promise.defer();
     const initPromise = initDeferred.promise;
-    return {
+
+    this.state = {
       map: null,
       options: {},
       API: null,
-      initPromise, initDeferred,
       position: null,
+      initPromise, initDeferred,
       bounds: new Bounds({
         minLat: null,
         maxLat: null,
@@ -48,54 +37,46 @@ export default React.createClass({
         maxLng: null,
       }),
     };
-  },
-  getDefaultProps() {
-    return {
-      apiKey: null,
-      daumAPILoading: <h1> 다음 지도 API를 로드하는 중입니다. </h1>,
-      daumAPILoadFailed: <h1> 다음 지도 API 로드를 실패하였습니다. </h1>,
-      onMove: ()=> {},
-    };
-  },
+  }
   componentDidMount() {
     const containerDiv = ReactDOM.findDOMNode(this.refs.containerDiv);
     ReactDOM.render(this.props.daumAPILoading, containerDiv);
-    daumAPIWrapper.loadPromise.then(()=> {
+    this.daumAPILoadPromise.then(() => {
       ReactDOM.unmountComponentAtNode(containerDiv);
-      this.state.API = daumAPIWrapper.getDaumMapAPI();
-      const daumLatLng = new this.state.API.LatLng(...this.props.position);
-      this.state.options = {
+      const daumAPI = daumAPIWrapper.getDaumMapAPI();
+      const daumLatLng = new daumAPI.LatLng(...this.props.position);
+      const options = {
         center: daumLatLng,
         level: 3,
         mapTypeId: convertToDaumBaseMapType(this.props.baseMapType),
       };
-      this.state.map = new this.state.API.Map(containerDiv, this.state.options);
+      const daumMap = new daumAPI.Map(containerDiv, options);
 
-      this.state.API.event.addListener(
-        this.state.map, 'center_changed', this.onMove);
-      const position = daumMapCoordsToArrayCoords(
-          this.state.map.getCenter());
-      const bounds = daumMapBoundsToMinMaxBounds(
-          this.state.map.getBounds());
+      daumAPI.event.addListener(
+        daumMap, 'center_changed', this.onMove);
+      const position = daumMapCoordsToArrayCoords(daumMap.getCenter());
+      const bounds = daumMapBoundsToMinMaxBounds(daumMap.getBounds());
+
       this.setState({
+        API: daumAPIWrapper.getDaumMapAPI(),
+        options,
+        map: daumMap,
         position,
         bounds,
       });
-
-
       this.state.initDeferred.resolve();
     })
-    .catch((rejection)=> {
+    .catch((rejection) => {
       console.error(rejection);
       ReactDOM.unmountComponentAtNode(containerDiv);
       ReactDOM.render(this.props.daumAPILoadFailed, containerDiv);
       this.state.initDeferred.reject(rejection);
     });
-  },
-  componentWillReceiveProps(nextProps) {
+  }
+  componentWillReceiveProps = (nextProps) => {
     const positionChanged = !_.isEqual(this.props.position, nextProps.position);
     if (positionChanged) {
-      this.state.initPromise.then(()=> {
+      this.initPromise.then(() => {
         this.state.map.setCenter(new this.state.API.LatLng(...nextProps.position));
         const position = daumMapCoordsToArrayCoords(
           this.state.map.getCenter());
@@ -106,7 +87,7 @@ export default React.createClass({
     }
     const zoomLevelChanged = !_.isEqual(this.props.zoomLevel, nextProps.zoomLevel);
     if (zoomLevelChanged) {
-      this.state.initPromise.then(()=> {
+      this.initPromise.then(() => {
         this.state.map.setLevel(nextProps.zoomLevel);
         const bounds = daumMapBoundsToMinMaxBounds(
           this.state.map.getBounds());
@@ -115,44 +96,77 @@ export default React.createClass({
     }
     const baseMapTypeChanged = !_.isEqual(this.props.baseMapType, nextProps.baseMapType);
     if (baseMapTypeChanged) {
-      this.state.initPromise.then(()=> {
+      this.state.initPromise.then(() => {
         this.state.map.setMapTypeId(convertToDaumBaseMapType(nextProps.baseMapType));
       });
     }
-  },
-  onMove() {
+  }
+  onMove = () => {
     const daumPosition = this.state.map.getCenter();
     const position = daumMapCoordsToArrayCoords(daumPosition);
     const bounds = daumMapBoundsToMinMaxBounds(
       this.state.map.getBounds());
     this.setState({ position, bounds });
     this.props.onMove(position);
-  },
+  }
   render() {
     return (
-      <div key="rootContainer" style={{...this.props.style, position: 'relative' }}>
-        <div key="container"
-             style={{
-                ...this.props.style,
-                position: 'absolute',
-                zIndex: 1
-             }}
-             ref="containerDiv"/>
-        <div key="overlays"
-             style={{
-               position: 'absolute',
-               zIndex: 2,
-               //overlay: 'hidden'
-             }}>
+      <div
+        key="rootContainer"
+        style={{ ...this.props.style, position: 'relative' }}
+      >
+        <div
+          key="container"
+          style={{
+            ...this.props.style,
+            position: 'absolute',
+            zIndex: 1,
+          }}
+          ref="containerDiv"
+        />
+        <div
+          key="overlays"
+          style={{
+            position: 'absolute',
+            zIndex: 2,
+          }}
+        >
           <OverlayContainer
-              position={this.state.position}
-              bounds={this.state.bounds}
-              width={this.props.style.width}
-              height={this.props.style.height}>
+            position={this.state.position}
+            bounds={this.state.bounds}
+            width={this.props.style.width}
+            height={this.props.style.height}
+          >
             {this.props.children}
           </OverlayContainer>
         </div>
       </div>
     );
-  },
-});
+  }
+}
+
+DaumMap.propTypes = {
+  children: React.PropTypes.oneOf([
+    React.PropTypes.array,
+    React.PropTypes.object,
+  ]),
+  APIKey: React.PropTypes.string,
+  daumAPILoading: React.PropTypes.node,
+  daumAPILoadFailed: React.PropTypes.node,
+  position: React.PropTypes.array.isRequired,
+  onMove: React.PropTypes.func,
+  style: React.PropTypes.object,
+  baseMapType: React.PropTypes.oneOf([
+    ..._.keys(DAUM_BASE_MAP_TYPES),
+    undefined,
+  ]),
+  overlayMapTypes: React.PropTypes.array,
+  zoomLevel: React.PropTypes.number,
+};
+
+DaumMap.defaultProps = {
+  apiKey: null,
+  daumAPILoading: <h1> 다음 지도 API를 로드하는 중입니다. </h1>,
+  daumAPILoadFailed: <h1> 다음 지도 API 로드를 실패하였습니다. </h1>,
+  onMove: () => {},
+};
